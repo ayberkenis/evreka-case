be97f012afb576ce19e53c77e80a6488cc0fb14f
ayberkenis <ayberkenis@gmail.com>
2024-11-12 17:34:20 +0300

    Added necessary API views/controllers for TCP.

--- case1/tcp_tracking/views.py
+++ case1/tcp_tracking/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import DeviceData
+from .serializers import DeviceDataSerializer
 
-# Create your views here.
+class DeviceDataListAPI(APIView):
+    def get(self, request):
+        # TODO: implement GET method
+        pass
+
+class LatestDeviceDataAPI(APIView):
+    def get(self, request, device_id):
+        # TODO: implement latest GET method
+        pass
