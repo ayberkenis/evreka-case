fd4e0fa6406cd7a08a2eab7b057dbba41819dabb
ayberkenis <ayberkenis@gmail.com>
2024-11-12 17:06:03 +0300

    Added pagination support for performance in tracking/data/list endpoint

--- case1/evreka_case1/settings.py
+++ case1/evreka_case1/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# REST FRAMEWORK SETTING
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50,  # Customize as needed
+}
 
 # Application definition
 
