44fdc20471be3b92816d1992c91622db29066b27
ayberkenis <ayberkenis@gmail.com>
2024-11-12 18:29:30 +0300

    Handled TCP socket data received async.

--- case1/tcp_tracking/tasks.py
+++ case1/tcp_tracking/tasks.py
@@ -1,16 +1,37 @@
 from celery import shared_task
-import json
+from django.utils import timezone
 from .models import DeviceData
-
+import logging
+logger = logging.getLogger(__name__)
 @shared_task
-def process_tcp_data(data):
+def process_tcp_data(data_list):
+    """
+    This function processes a list of device data and stores them in the database.
+
+    Parameters:
+    data_list (list): A list of dictionaries, where each dictionary represents a device data.
+                      The dictionary should have the following keys: 'device_id', 'location', 'speed', and optionally 'timestamp'.
+                      If 'timestamp' is not provided, the current time will be used.
+
+    Returns:
+    None
+
+    Raises:
+    Exception: If any error occurs during the processing of device data.
+    """
     try:
-        data_dict = json.loads(data)
-        DeviceData.objects.create(
-            device_id=data_dict.get('device_id'),
-            location=data_dict.get('location'),
-            speed=data_dict.get('speed'),
-            # Include other fields as needed
-        )
+
+        device_data_instances = [
+            DeviceData(
+                device_id=data['device_id'],
+                location=data['location'],
+                speed=data['speed'],
+                timestamp=data['timestamp'] if 'timestamp' in data else timezone.now()
+            )
+            for data in data_list
+        ]
+        DeviceData.objects.bulk_create(device_data_instances)
+        logger.info(f"Successfully processed {len(device_data_instances)} device data.")
     except Exception as e:
-        print(f"Error processing data: {e}")
+        logger.error(f"Error processing device data: {e}")
+        raise
